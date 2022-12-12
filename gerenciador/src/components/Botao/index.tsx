5f466a4bc5acc59282fb047feaae2ddbc2d6b17a
lucas.pires <lucas.pires@softplan.com.br>
2022-12-11 22:03:43 -0300

    Inserindo funcionalidade de cadastro de atividades

--- gerenciador/src/components/Botao/index.tsx
+++ gerenciador/src/components/Botao/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import style from './Botao.module.scss'
 
-class Botao extends React.Component<{ texto: string }> {
+class Botao extends React.Component<{ 
+    texto: string, 
+    type?: "button" | "submit" | "reset" | undefined }
+    > {
     render() {
+        const { texto, type = "button" } = this.props;
         return (
-            <button className={style.botao}>
-                {this.props.texto}
+            <button className={style.botao} type={type}>
+                {texto}
             </button>
         )
     }
