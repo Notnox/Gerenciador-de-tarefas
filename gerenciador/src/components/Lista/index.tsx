32e50c91452dc7fe35e8bd2b77dc8919c1d5eb0b
lucas.pires <lucas.pires@softplan.com.br>
2022-12-11 22:30:39 -0300

    Inserindo funcionalidade de selecao de item

--- gerenciador/src/components/Lista/index.tsx
+++ gerenciador/src/components/Lista/index.tsx
@@ -2,17 +2,23 @@ import { ITarefa } from "../../types/tarefa";
 import Item from "./Item";
 import style from './Lista.module.scss'
 
-function Lista({ tarefas }: { tarefas: ITarefa[]}) {
+interface Props {
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
     return (
         <aside className={style.listaTarefas}>
             <h2>
                 Estudos do dia
             </h2>
             <ul>
-                {tarefas.map((t, index) => (
+                {tarefas.map((t) => (
                     <Item 
-                        key={index}
+                        key={t.id}
                         {...t}
+                        selecionaTarefa={selecionaTarefa}
                     />
                 ))}
             </ul>
--- gerenciador/src/pages/App.tsx
+++ gerenciador/src/pages/App.tsx
@@ -7,10 +7,23 @@ import { ITarefa } from '../types/tarefa';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecinado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefas => tarefas.map(tarefa => ({
+      ...tarefa, 
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })));
+  }
+
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas}/>
-      <Lista tarefas={tarefas}/>
+      <Lista 
+        tarefas={tarefas} 
+        selecionaTarefa={selecionaTarefa}
+      />
       <Cronometro />
     </div>
   );
