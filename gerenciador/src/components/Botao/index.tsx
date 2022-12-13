595c11f8f1e2a8eb85977df63395246fc84c1151
lucas.pires <lucas.pires@softplan.com.br>
2022-12-12 21:59:50 -0300

    Refatorando botao e formulario para function component

--- gerenciador/src/components/Botao/index.tsx
+++ gerenciador/src/components/Botao/index.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import style from './Botao.module.scss'
 
-class Botao extends React.Component<{ 
+interface Props {
     texto: string, 
     type?: "button" | "submit" | "reset" | undefined ,
     onClick?: () => void
-    }> {
-    render() {
-        const { texto, type = "button", onClick } = this.props;
-        return (
-            <button 
-                className={style.botao} 
-                type={type}
-                onClick={onClick}
-            >
-                {texto}
-            </button>
-        )
-    }
+}
+
+function Botao({onClick, type, texto}: Props) {
+    return (
+        <button 
+            className={style.botao} 
+            type={type}
+            onClick={onClick}
+        >
+            {texto}
+        </button>
+    )
 }
 
 export default Botao;
